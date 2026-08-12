1abd0555c478234e97f05fa00fe93988ff36499a
jhonjaider438 <jhonjaider438@gmail.com>
2026-08-12 11:46:17 -0500

    trabajando en la pagina de bienvenida flutter

---
+++ Fronted/fronted/lib/paginas/bienvenida.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class bienvenida extends StatefulWidget {
+  const bienvenida({super.key});
+
+  @override
+  State<bienvenida> createState() => _bienvenidaState();
+}
+
+class _bienvenidaState extends State<bienvenida> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Bienvenido a Megatech 2'),
+          Text(
+            'En Megatech 2 te ofrecemos lo mejor en tecnología con una experiencia rápida, segura y confiable para que compres sin complicaciones.',
+          ),
+        ],
+      ),
+    );
+  }
+}
